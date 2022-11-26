ef8a0eb112a2ce07c4aad54d138e28bed6097c23
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2022-11-26 21:21:28 +0200

    Event Delegation
    -eveniment setat pentru un element cu mai multi copii
    -asa putem modifica la alegere copii elementului parinte
    
    -crearea unui element nou si adaugarea lui in eveniment

--- code.js
+++ code.js
@@ -1,57 +1,67 @@
-//Event Propagation
+//Event delegation
 
-/*
-window.addEventListener  ("click", function () { 
-    console.log ("window");
-},true);
+// @@@--> Modul cu propagare
+/* document.querySelector ('#film-1').addEventListener ("click", function (e) {
+    console.log ("Spider Man 3");
 
-document.addEventListener ("click", function () {
-    console.log ("document");
-}, true);
+    const target = e.target;
 
-document.querySelector('.div-2').addEventListener("click", function (e) { 
-    // e.stopPropagation ();  ---> opreste de tot
-    console.log ("Div 2");
-}, {once: true});
-
-
-document.querySelector (".div-1").addEventListener("click", function () {
-    console.log ("Div 1");
-},true);
-
-document.querySelector("button").addEventListener ("click", function (e) {
-    console.log (e.target.innerText = 'clicked'); // e - obiectul pentru eveniment
-}, true); */
-
-//-----
-window.addEventListener  ("click", function () { 
-    console.log ("window");
-},false);
-
-document.addEventListener ("click", function () {
-    console.log ("document");
-}, false);
-
-document.querySelector('.div-2').addEventListener("click", function () { 
-    console.log ("Div 2");
-}, {once: true});
-
-
-document.querySelector (".div-1").addEventListener("click", function () {
-    console.log ("Div 1");
-},false);
-
-document.querySelector("button").addEventListener ("click", function (e) {
-    console.log (e.target.innerText = 'clicked'); // e - obiectul pentru eveniment
-}, false);
-
-//Daca e fals - propagarea se face de la obiect la sursa
-//Daca e adevarat - propagarea se face in ordinea dorita   
-//e.stopPropagation () - in ambele sensuri 
-
-
-//PREVENT DEFAULT METHOD - ex. for anchor tag
-document.querySelector(".btn-default").addEventListener ("click", function (e) { 
-    e.preventDefault ();
-    console.log (e.target.innerText = "clicked-1");
+    if (target.matches ("li")) { 
+        target.style.backgroundColor = "lightgrey";
+    }
 })
+
+document.querySelector ('#film-2').addEventListener ("click", function (e) {
+    console.log ("Iron Man 2");
+
+    const target = e.target;
+    
+    if (target.matches ("li")) { 
+        target.style.backgroundColor = "lightgrey";
+    }
+})
+
+document.querySelector ('#film-3').addEventListener ("click", function (e) {
+    console.log ("Peaky Blinders");
+
+    const target = e.target;
+    
+    if (target.matches ("li")) { 
+        target.style.backgroundColor = "lightgrey";
+    }
+})
+
+document.querySelector ('#film-4').addEventListener ("click", function (e) {
+    console.log ("Triunghiul Mortii");
+
+    const target = e.target;
+    
+    if (target.matches ("li")) { 
+        target.style.backgroundColor = "lightgrey";
+    }
+}) */
+
+//Nota Personala: e este foarte folosit cand vrei sa modifici elementul din HTML
+
+// @@@--> Event Delegation
+//Varianta scurta   
+const ul = document.querySelector ("#films");
+
+
+ul.addEventListener ("click", function(e) { //practic setam un eveniment pentru intreaga lista 
+    
+    console.log (e.target.getAttribute('id') + ' is clicked');
+
+    const target = e.target;
+    if (target.matches('li')) { 
+        target.style.backgroundColor = "lightgrey";
+    };
+});
+
+const newFilms = document.createElement ('li');
+
+//Adaugarea la eveniment a altui obiect
+newFilms.innerText = "Titanic";
+newFilms.setAttribute ('id', 'film-5');
+
+ul.appendChild (newFilms);  
